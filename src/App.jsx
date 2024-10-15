f61a48956ebb0b37ac3f432c2126a96b04d143d5
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2024-10-15 17:32:38 +0530

    navbar gui

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ import Display from './components/Display'
 const App = () => {
   return (
     <div className='h-screen bg-black'>
-      <div className='h-[90%] flex '>
+      <div className='h-[90%] flex'>
         <Sidebar />
         <Display />
       </div>
       <Player />
     </div>
   )
-}
+};
 
 export default App
