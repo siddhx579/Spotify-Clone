bbcc0f354313fa7371e8a55c5bc60f06e80eb8b0
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2024-11-03 22:47:09 +0530

    Update albumModel.js

--- Backend/src/models/albumModel.js
+++ Backend/src/models/albumModel.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 const albumSchema = new mongoose.Schema({
     name: { type: String, required: true },
     desc: { type: String, required: true },
-    bgColor: { type: String, required: true },
-    image: { type: String, required: true }
-});
+    bgColour: { type: String, required: true },
+    image: { type: String, required: true },
+})
 
 const albumModel = mongoose.models.album || mongoose.model("album", albumSchema);
 
