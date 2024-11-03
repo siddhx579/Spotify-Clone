3099b486a1566808347806331d8f49771905d91a
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2024-11-03 22:47:03 +0530

    update

--- Backend/src/config/mongodb.js
+++ Backend/src/config/mongodb.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+
     mongoose.connection.on('connected', () => {
-        console.log('Connected to MongoDB');
+        console.log("Connected to MongoDB")
     });
-    await mongoose.connect(`${process.env.MONGODB_URI}/spotify`);
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/spotify`)
+
 }
 
 export default connectDB;
