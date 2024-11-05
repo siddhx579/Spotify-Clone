9ec870b7c1df3bfee5dd23aff8606f9adbcadb40
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2024-11-05 17:13:16 +0530

    app update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,23 +1,30 @@
 import React, { useContext } from 'react'
-import { Sidebar } from './components/Sidebar'
+import Sidebar from './components/Sidebar'
 import Player from './components/Player'
 import Display from './components/Display'
-import { PlayerContext } from './context/PlayerContext';
+import { PlayerContext } from './context/PlayerContext'
 
 const App = () => {
 
-  const {audioRef, track} = useContext(PlayerContext);
+  const { audioRef, track, songsData } = useContext(PlayerContext);
 
   return (
     <div className='h-screen bg-black'>
-      <div className='h-[90%] flex'>
-        <Sidebar />
-        <Display />
-      </div>
-      <Player />
-      <audio ref={audioRef} src={track.file} preload='auto'></audio>
+      {
+        songsData.length !==0
+          ? <>
+            <div className='h-[90%] flex'>
+              <Sidebar />
+              <Display />
+            </div>
+            <Player />
+          </>
+          : ""
+      }
+
+      <audio ref={audioRef} src={track?track.file:""} preload='auto'></audio>
     </div>
   )
-};
+}
 
 export default App
